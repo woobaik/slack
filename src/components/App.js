ceed389dab34d90ffe8cc891928c00e19421710f
Jbaik <joungwoo.baik@gmail.com>
2020-04-26 15:14:51 -0700

    Add firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Login from './auth/Login.component'
+import Register from './auth/Register.component'
+
+
 function App() {
   return (
-    <div className="App">
-      APP component
-    </div>
+      <Router>
+        <div className="App">
+            <header>
+              Hello its header
+            </header>
+            <Switch>
+              <Route path='/login' component={Login} />
+              <Route path='/register' component={Register} />
+            </Switch>
+        </div>
+      </Router>
   );
 }
 
