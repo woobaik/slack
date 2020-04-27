ba4a733af5a5f091ce73c3a4864e78517845a8bd
Jbaik <joungwoo.baik@gmail.com>
2020-04-26 18:54:03 -0700

    Added basic authenticate function to the app

--- src/components/auth/Register.component.jsx
+++ src/components/auth/Register.component.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import firebase from '../../firebase'
 
 const Register = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(email, password, passwordConfirmation)
+
+        if (password === passwordConfirmation) {
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch( error => {
+                console.log('Error Code', error.code)
+                console.log('Error Msg', error.message)
+            })
+        } else {
+            console.log('Password is not matching please try again')
+        }
+        
+    }
+
     return (
         <div>
-            Register
+         
+           <form onSubmit={handleSubmit}>
+                <div>
+                    <input name='email' onChange={event => setEmail(event.target.value)} value={email} type='text' />
+               </div>
+               <div>
+                   <input name='password' onChange={event=> setPassword(event.target.value)} value={password} type='password'   />
+               </div>
+               <div>
+                   <input name='passwordConfirmation' onChange={event=> setPasswordConfirmation(event.target.value)}  value={passwordConfirmation} type='password' />
+               </div>
+               <button>라라라라</button>
+           </form>
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+
+
+import "firebase/auth";
+import "firebase/firestore";
+
+const config = {
+ 
+    apiKey: "AIzaSyA9MXwhKKzkFMWlwHPJaqHF-Q3-08pm12k",
+    authDomain: "slack-clone-8850d.firebaseapp.com",
+    databaseURL: "https://slack-clone-8850d.firebaseio.com",
+    projectId: "slack-clone-8850d",
+    storageBucket: "slack-clone-8850d.appspot.com",
+    messagingSenderId: "605480570290",
+    appId: "1:605480570290:web:fda935d5cfcba1f78ea8ab",
+    measurementId: "G-CQGDMYHTWL"
+}
+
+firebase.initializeApp(config);
+
+export default firebase
