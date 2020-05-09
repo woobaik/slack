012f422c22a8d1e7e4e65d1b29adb3180c7f771c
Jbaik <joungwoo.baik@gmail.com>
2020-05-09 01:46:28 -0700

    Add basic layout and folder structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,26 @@
 import React from "react"
+import SideBar from "./mainChatScreen/sideBar/SideBar.component"
+import Chat from "./mainChatScreen/chat/Chat.component"
+import Meta from "./mainChatScreen/meta/MetaPage.compoent"
 
+import { Grid, Image } from "semantic-ui-react"
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<Grid columns={3} celled>
+			<Grid.Column
+				width={3}
+				style={{ backgroundColor: "#3f0e40", height: "94vh" }}
+				stretched>
+				<SideBar />
+			</Grid.Column>
+			<Grid.Column width={10}>
+				<Chat />
+			</Grid.Column>
+			<Grid.Column width={3}>
+				<Meta />
+			</Grid.Column>
+		</Grid>
+	)
 }
 
 export default Home
---
+++ src/components/mainChatScreen/meta/MetaPage.compoent.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const MetaPage = () => {
+	return <div>MetaPage</div>
+}
+
+export default MetaPage
